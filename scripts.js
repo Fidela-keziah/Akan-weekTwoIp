6d55342f14046824b801f651eed84dd11276d855
Fidela-Keziah <irafidela@gmail.com>
2021-01-11 17:15:12 +0200

    add file

---
+++ scripts.js
@@ -0,0 +1,21 @@
+function getname() {
+    var gender = document.getElementById("gender").checked;
+    var day = document.getElementById("birthday").value;
+    var date = new Date(day).getDay();
+    if (day && gender !== null) {
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][date];
+        if (gender == true) {
+            var names = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+            var getname = names[date];
+        }
+        else {
+
+            var names = ["Kwasi", "Kwadwo", "kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+            var getname = names[date];
+        }
+        alert("Hi, Your Akan Name is " + getname + ". the day you were born is " + days);
+    }
+    else {
+        alert("Enter a date and select gender");
+    }
+}
